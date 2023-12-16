4609afca7b09889c822938d3cc43e4715797f606
Adem Hatay <hatayadem5@gmail.com>
2023-12-16 17:24:28 +0300

    update

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,9 +1,9 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server'
-import { clerkClient } from '@clerk/nextjs'
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
+import { clerkClient } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
  
 export async function POST(req: Request) {
  
@@ -54,7 +54,6 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
  
-
   if(eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
@@ -102,11 +101,6 @@ export async function POST(req: Request) {
 
     return NextResponse.json({ message: 'OK', user: deletedUser })
   }
-
-
-
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
-  console.log('Webhook body:', body)
  
   return new Response('', { status: 200 })
 }
